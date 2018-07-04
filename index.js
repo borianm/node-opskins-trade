0f3ca6061649418914bcc6c2c0fb1827fc5caadc
Borian Markovic <borian.markovic@gmail.com>
2018-07-04 16:14:48 +0200

    Revert "Polling bugfixes and useful changes"

--- index.js
+++ index.js
@@ -11,72 +11,71 @@ const Case = require('./libs/classes/Case');
 const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
 
 class OPSkinsTrade extends EventEmitter {
-    constructor(apiKey, secret, polling = 1000) {
-        if (!apiKey || !secret) {
-            throw new Error(`Please provide a valid ${apiKey ? 'secret' : 'API key'}`);
+  constructor(apiKey, secret, polling = 1000) {
+    if (!apiKey || !secret) {
+      throw new Error(`Please provide a valid ${apiKey ? 'secret' : 'API key'}`);
+    }
+    super();
+    this.request = new Request(apiKey);
+    this.Item = new Item(this.request);
+    this.Trade = new Trade(this.request);
+    this.User = new User(this.request);
+    this.Ethereum = new Ethereum(this.request);
+    this.CaseSite = new CaseSite(this.request);
+    this.Case = new Case(this.request);
+    this.api_key = apiKey;
+    this.secret = secret;
+    this.polling = polling;
+    this.pollData = {};
+  }
+
+  generateTwoFactor() {
+    return authenticator.generateToken(this.secret);
+  }
+
+  async pollTrades() {
+    const fetchTrades = await this.Trade.getOffers();
+    const { offers } = fetchTrades.response;
+
+    offers.forEach((offer) => {
+      if (offer.id in this.pollData) {
+        if (this.pollData[offer.id] !== offer.state) {
+          this.emit('offerUpdated', offer);
         }
-        super();
-        this.request = new Request(apiKey);
-        this.Item = new Item(this.request);
-        this.Trade = new Trade(this.request);
-        this.User = new User(this.request);
-        this.Ethereum = new Ethereum(this.request);
-        this.CaseSite = new CaseSite(this.request);
-        this.Case = new Case(this.request);
-        this.api_key = apiKey;
-        this.secret = secret;
-        this.polling = polling;
-        this.pollData = {};
-    }
+        return;
+      }
+      this.pollData[offer.id] = offer.state;
+      this.emit('newOffer', offer);
+    });
 
-    generateTwoFactor() {
-        return authenticator.generateToken(this.secret);
-    }
+    await sleep(this.polling);
+    this.pollTrades();
+  }
 
-    async pollTrades() {
-        const fetchTrades = await this.Trade.getOffers();
-        const {offers} = fetchTrades.response;
+  getUserInventory(steamid) {
+    return this.Trade.getUserInventoryFromSteamId({ steam_id: steamid, app_id: '2' });
+  }
 
-        offers.forEach((offer) => {
-            if (offer.id in this.pollData) {
-                if (this.pollData[offer.id] !== offer.state) {
-                    this.pollData[offer.id] = offer.state;
-                    this.emit('offerUpdated', offer);
-                }
-                return;
-            }
-            this.pollData[offer.id] = offer.state;
-            this.emit('newOffer', offer);
-        });
+  async sendOffer(steamid, items) {
+    const sendOffer = await this.Trade.sendOfferToSteamId({
+      twofactor_code: this.generateTwoFactor(),
+      steam_id: steamid,
+      items,
+    });
+    const { offer } = sendOffer.response;
+    this.pollData[offer.id] = offer.state;
+    this.emit('sentOffer', offer);
+  }
 
-        await sleep(this.polling);
-        this.pollTrades();
-    }
-
-    getUserInventory(steamid, app_id) {
-        return this.Trade.getUserInventoryFromSteamId({steam_id: steamid, app_id});
-    }
-
-    async sendOffer(steamid, items) {
-        const sendOffer = await this.Trade.sendOfferToSteamId({
-            twofactor_code: this.generateTwoFactor(),
-            steam_id: steamid,
-            items,
-        });
-        const {offer} = sendOffer.response;
-        this.pollData[offer.id] = offer.state;
-        this.emit('sentOffer', offer);
-    }
-
-    async acceptOffer(offerId) {
-        const acceptOffer = await this.Trade.acceptOffer({
-            twofactor_code: this.generateTwoFactor(),
-            offer_id: offerId,
-        });
-        const {offer} = acceptOffer.response;
-        this.pollData[offer.id] = offer.state;
-        this.emit('offerUpdated', offer);
-    }
+  async acceptOffer(offerId) {
+    const acceptOffer = await this.Trade.acceptOffer({
+      twofactor_code: this.generateTwoFactor(),
+      offer_id: offerId,
+    });
+    const { offer } = acceptOffer.response;
+    this.pollData[offer.id] = offer;
+    this.emit('offerUpdated', offer);
+  }
 }
 
 module.exports = OPSkinsTrade;
